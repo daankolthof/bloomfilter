fd077bb951713c74aba9f3ad95f0d7673f061047
Daan Kolthof <daan_kolthof@hotmail.com>
2019-05-25 18:23:37 +0100

    Changed the Bloom filter to use MD5 as hash function instead of C++ internal std::hash as MD5 has a uniform distribution of hash results, resulting in fewer false positives when doing lookup.

--- bloomfilter/bloomfilter.h
+++ bloomfilter/bloomfilter.h
@@ -1,38 +1,25 @@
 #ifndef BLOOMFILTER_H
 #define BLOOMFILTER_H
 
-#include <bitset>
-#include <chrono>
-#include <random>
+#include <openssl/md5.h>
 
-template <typename T>
+#include <bitset>
+#include <memory>
+
 class Bloomfilter
 {
 public:
-	typedef T value_type;
-
 	Bloomfilter()
-	: object_count_(0)
+	: object_count_(0) {}
+
+	void insert(const std::string& object)
 	{
-		const uint64_t generator_seed = std::chrono::system_clock::now().time_since_epoch().count();
-		std::mt19937_64 generator(generator_seed);
-		std::uniform_int_distribution<size_t> distribution;
+		const std::unique_ptr<unsigned char[]> object_md5_hash = hash(object);
+		const uint16_t* const object_hashes = reinterpret_cast<const uint16_t * const>(object_md5_hash.get());
 
 		for (size_t i = 0; i < hash_function_count; i++)
 		{
-			bitmasks[i] = distribution(generator);
-		}
-	}
-
-	~Bloomfilter()
-	{}
-
-	void insert(const T& object)
-	{
-		const size_t object_hash = hash(object);
-		for (size_t i = 0; i < hash_function_count; i++)
-		{
-			const size_t index_to_set = (object_hash ^ bitmasks[i]) % bloomfilter_store_size;
+			const uint16_t index_to_set = object_hashes[i];
 			bloomfilter_store_[index_to_set] = true;
 		}
 		++object_count_;
@@ -44,12 +31,14 @@ public:
 		object_count_ = 0;
 	}
 
-	bool contains(const T& object) const
+	bool contains(const std::string& object) const
 	{
-		const size_t object_hash = hash(object);
+		const std::unique_ptr<unsigned char[]> object_md5_hash = hash(object);
+		const uint16_t* const object_hashes = reinterpret_cast<const uint16_t * const>(object_md5_hash.get());		
+
 		for (size_t i = 0; i < hash_function_count; i++)
 		{
-			const size_t index_to_get = (object_hash ^ bitmasks[i]) % bloomfilter_store_size;
+			const uint16_t index_to_get = object_hashes[i];
 			if (!bloomfilter_store_[index_to_get]) return false;
 		}
 		return true;
@@ -67,17 +56,29 @@ public:
 
 private:
 
-	static const size_t bloomfilter_store_size = 8192 * 8; // Size of the bloom filter state in bits.
-	static const size_t hash_function_count = 6; // Number of hash functions to use when hashing objects.
+	// Size of the MD5 hash result, always fixed to 16 bytes.
+	static const size_t MD5_result_size_bytes = 16;
+	
+	// Size of the bloom filter state in bits (2^16).
+	static const size_t bloomfilter_store_size = 65536;
 
-	inline
-	static size_t hash(const T& val)
+	// Number of hash functions to use when hashing objects (cannot be larger than MD5_result_size_bytes/bytes_per_hash_function).
+	static const size_t hash_function_count = 4;
+
+	// Set to 2 bytes so all bloom filter bits can be indexed (2^16 different values).
+	static const size_t bytes_per_hash_function = 2;
+
+	static std::unique_ptr<unsigned char[]> hash(const std::string& val)
 	{
-		return std::hash<T>()(val);
+		std::unique_ptr<unsigned char[]> result = std::make_unique<unsigned char[]>(MD5_result_size_bytes);
+		const unsigned char* const md5_input_val = reinterpret_cast<const unsigned char*>(val.data());
+		const size_t md5_input_length = val.length();
+		MD5(md5_input_val, md5_input_length, result.get());
+
+		return result;
 	}
 
 	std::bitset<bloomfilter_store_size> bloomfilter_store_;
-	size_t bitmasks[hash_function_count];
 
 	size_t object_count_;
 };
